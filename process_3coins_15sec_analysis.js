ed53ec231a43478f96e9aebca1592dfafd84f1fe
Martin <bekzodsanakulov89@gmail.com>
2021-07-15 11:01:37 +0900

    feat: eliminating the special double digit amounts

--- process_3coins_15sec_analysis.js
+++ process_3coins_15sec_analysis.js
@@ -111,7 +111,7 @@ function cumulateDigits(ele, key, coin_type) {
 
     let summary = first_digit + second_digit;
 
-    if(first_digit === second_digit) {
+    if(first_digit === second_digit && false) {
         switch (coin_type) {
             case 'bitcoin':
                 doubles_btc[`${first_digit}${second_digit}`]++;
