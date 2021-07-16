3fa25fceaf23e83f650dd1abbe74a10a641437b4
Martin <bekzodsanakulov89@gmail.com>
2021-07-16 15:08:37 +0900

    feat: fireman logic change by odd even of summary logic

--- process_3coins_5mx20s_analysis.js
+++ process_3coins_5mx20s_analysis.js
@@ -108,7 +108,8 @@ function shapingTime(value) {
 
 // CUMULATIVE SUMMARY CALCULATION
 function declaringResults(ele, key, coin_type) {
-    let numb = Math.round(ele.close * 100) % 100;
+    let total_numb = Math.round(ele.close * 100) % 100;
+    let numb = Math.trunc(total_numb / 10) + total_numb % 10;
     let result = (numb % 2 === 0) ? 'even' : 'odd';
     if (coin_type === 'bitcoin') {
         target_btc[`${key}`] = {result: result, data: ele};
